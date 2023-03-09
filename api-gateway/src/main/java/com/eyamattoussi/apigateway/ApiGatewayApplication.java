e3dbfe14a614e1ce804de7d45b5a63c292138d66
eyaMattoussi <eya.mattoussii@gmail.com>
2023-03-09 21:26:14 +0100

    api gateway

--- api-gateway/src/main/java/com/eyamattoussi/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/eyamattoussi/apigateway/ApiGatewayApplication.java
@@ -1,14 +1,13 @@
-package com.eyamattoussi.eurekadiscovery;
+package com.eyamattoussi.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableEurekaServer
-public class EurekaDiscoveryApplication {
+public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaDiscoveryApplication.class, args);
+		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
 }
